07252dc726ae7f3363a5e645a0f546d1aaf37c87
edeane <edward.deane.ii@gmail.com>
2017-11-08 16:14:20 -0700

    poke

--- pokedex.py
+++ pokedex.py
@@ -13,6 +13,9 @@ import requests
 import mahotas
 import os
 import pickle
+from skimage import exposure
+from scipy.spatial import distance
+
 
 def scrape_poke():
     url = 'https://pokemondb.net/pokedex/national'
@@ -80,22 +83,107 @@ def create_index():
         thresh = cv2.bitwise_not(gray)
         thresh[thresh>10] = 255
         thresh[thresh<10] = 0
-        cv2.imshow('thresh', thresh)
         outline = np.zeros(thresh.shape, dtype='uint8')
         (_, cnts, _) = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
         cv2.drawContours(outline, [cnts], -1, (255, 255, 255), -1)
-        cv2.imshow('a{}'.format(i), np.hstack([gray, thresh, outline]))
         moments = desc.describe(outline)
         index[poke] = moments
     
     pickle.dump(index, open('images/pokedex/index.pkl', 'wb'))
+    
+def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+	dim = None
+	(h, w) = image.shape[:2]
+	if width is None and height is None:
+		return image
+	if width is None:
+		r = height / float(h)
+		dim = (int(w * r), height)
+	else:
+		r = width / float(w)
+		dim = (width, int(h * r))
+	resized = cv2.resize(image, dim, interpolation = inter)
+	return resized
+
+
+def get_gameboy():
+    image = cv2.imread('images/pokedex/gameboy.jpg')
+    ratio = image.shape[0] / 300
+    orig = image.copy()
+    image = resize(image, height=300)
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    bifil = cv2.bilateralFilter(gray, 11, 17, 17) #similar to blur but keeps edges better
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    edged_bifil = cv2.Canny(bifil, 20, 250)
+    (_, cnts, _) = cv2.findContours(edged_bifil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse=True)[:100]
+    screenCnt = None
+    for i, cnt in enumerate(cnts):
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+        print(i)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    cv2.drawContours(image, [screenCnt], -1, (255,0,0), 2)
+    pts = screenCnt.reshape(4,2)
+    rect = np.zeros((4,2), dtype='float32')
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    rect *= ratio
+    tl, tr, br, bl = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([[0,0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]], dtype='float32')
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+    warp_before = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+    warp_after = exposure.rescale_intensity(warp_before, out_range=(0,255))
+    h, w = warp_after.shape
+    dX, dY = (int(w*.4), int(h*.45))
+    crop = warp_after[10:dY, w-dX:w-10]
+    cv2.imshow('crop', crop)
+    cv2.imwrite('images/pokedex/gameboy_guy.png', crop)
+
+class Searcher():
+    def __init__(self, index):
+        self.index = index
+    def search(self, queryFeatures):
+        results = {}
+        for k, features in self.index.items():
+            d = distance.euclidean(queryFeatures, features)
+            results[k] = d
+        results = sorted([(v,k) for k,v in results.items()])
+        return results
 
 
 
-
-
-
+def show_xy():
+    xy = np.zeros((300, 500, 3))
+    locations = ((10,20), (390,290), (10,290), (390,20))
+    for loc in locations:
+        cv2.putText(xy, str(loc), loc, cv2.FONT_HERSHEY_SIMPLEX, .6, (255,255,255), 2)
+    cv2.imshow('xy', xy)
+    
 
 if __name__ == '__main__':
-    create_index()
+    show_xy()
+    get_gameboy()
+    
+    
+    
+    
+    
+    
+    
+    
